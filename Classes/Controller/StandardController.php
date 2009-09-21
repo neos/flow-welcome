48c8a099702f7099fb7465e643648db5e0f619db
Karsten Dambekalns <karsten@typo3.org>
2009-09-21 20:27:28 +0000

    [+BUGFIX] Welcome: fixed command line instructions on welcome screen, fixes #4551, fixes #4489.

--- Classes/Controller/StandardController.php
+++ Classes/Controller/StandardController.php
@@ -46,7 +46,8 @@ class StandardController extends \F3\FLOW3\MVC\Controller\ActionController {
 	 */
 	public function indexAction() {
 		$this->view->assign('baseURI', $this->request->getBaseURI());
-		$this->view->assign('flow3CommandLinePath', realpath(FLOW3_PATH_ROOT));
+		$this->view->assign('flow3PathRoot', realpath(FLOW3_PATH_ROOT));
+		$this->view->assign('flow3PathWeb', realpath(FLOW3_PATH_WEB));
 		$this->view->assign('isWindows', $this->isWindows());
 
 		$flow3Package = $this->packageManager->getPackage('FLOW3');
