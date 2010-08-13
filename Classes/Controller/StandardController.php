22926e1585cc1cf34863ae6d3fc0a6c151c2db34
Karsten Dambekalns <karsten@typo3.org>
2010-08-13 17:02:00 +0200

    [-TASK] Welcome: Removed @version keyword from files, relates to #8835.
    Change-Id: I0eb979fe055d416d8848f3849eff79728a31e7cb

--- Classes/Controller/StandardController.php
+++ Classes/Controller/StandardController.php
@@ -25,7 +25,6 @@ namespace F3\Welcome\Controller;
 /**
  * Controller with a welcome start screen for FLOW3
  *
- * @version $Id$
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
  */
 class StandardController extends \F3\FLOW3\MVC\Controller\ActionController {
