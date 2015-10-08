0d28a4b34ce20d2d242e4815f3b36e40c9cc9fc1
Karsten Dambekalns <karsten@dambekalns.de>
2015-10-08 12:42:49 +0200

    [TASK] Make controller PSR-2 compliant

--- Classes/TYPO3/Welcome/Controller/StandardController.php
+++ Classes/TYPO3/Welcome/Controller/StandardController.php
@@ -17,45 +17,45 @@ use TYPO3\Flow\Annotations as Flow;
  * Controller with a welcome start screen for Flow
  *
  */
-class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController {
+class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController
+{
+    /**
+     * @var \TYPO3\Flow\Package\PackageManagerInterface
+     * @Flow\Inject
+     */
+    protected $packageManager;
 
-	/**
-	 * @var \TYPO3\Flow\Package\PackageManagerInterface
-	 * @Flow\Inject
-	 */
-	protected $packageManager;
+    /**
+     * Index action
+     *
+     * @return void
+     */
+    public function indexAction()
+    {
+        $this->view->assign('flowPathRoot', realpath(FLOW_PATH_ROOT));
+        $this->view->assign('flowPathWeb', realpath(FLOW_PATH_WEB));
+        $this->view->assign('isMyPackageActive', $this->packageManager->isPackageActive('MyCompany.MyPackage'));
 
-	/**
-	 * Index action
-	 *
-	 * @return void
-	 */
-	public function indexAction() {
-		$this->view->assign('flowPathRoot', realpath(FLOW_PATH_ROOT));
-		$this->view->assign('flowPathWeb', realpath(FLOW_PATH_WEB));
-		$this->view->assign('isMyPackageActive', $this->packageManager->isPackageActive('MyCompany.MyPackage'));
+        $baseUri = $this->request->getHttpRequest()->getBaseUri();
+        $this->view->assign('baseUri', $baseUri);
 
-		$baseUri = $this->request->getHttpRequest()->getBaseUri();
-		$this->view->assign('baseUri', $baseUri);
+        $this->view->assign('isWindows', DIRECTORY_SEPARATOR !== '/');
 
-		$this->view->assign('isWindows', DIRECTORY_SEPARATOR !== '/');
+        $flowPackage = $this->packageManager->getPackage('TYPO3.Flow');
+        $version = $flowPackage->getPackageMetaData()->getVersion();
+        $this->view->assign('version', $version);
 
-		$flowPackage = $this->packageManager->getPackage('TYPO3.Flow');
-		$version = $flowPackage->getPackageMetaData()->getVersion();
-		$this->view->assign('version', $version);
+        $activePackages = $this->packageManager->getActivePackages();
+        $this->view->assign('activePackages', $activePackages);
 
-		$activePackages = $this->packageManager->getActivePackages();
-		$this->view->assign('activePackages', $activePackages);
+        $this->view->assign('notDevelopmentContext', !$this->objectManager->getContext()->isDevelopment());
+    }
 
-		$this->view->assign('notDevelopmentContext', !$this->objectManager->getContext()->isDevelopment());
-	}
-
-	/**
-	 * @return void
-	 */
-	public function redirectAction() {
-		$this->redirect('index');
-	}
+    /**
+     * @return void
+     */
+    public function redirectAction()
+    {
+        $this->redirect('index');
+    }
 }
-
-?>
